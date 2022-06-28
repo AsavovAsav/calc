eb7c69be9bdb3813a146a1d5362cb42a21a4dbf3
AsavovAsav <mr.khalinov40@gmail.com>
2022-06-28 21:54:01 +0600

    calc 1.5
    Работа с помощью switch

--- calc/Form1.cs
+++ calc/Form1.cs
@@ -12,6 +12,9 @@ namespace calc
 {
     public partial class Form1 : Form
     {
+        public string action;
+        public double sum;
+        public double first, sec;
         public Form1()
         {
             InitializeComponent();
@@ -19,42 +22,58 @@ namespace calc
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double sum;
-            double first, sec;
-            first=Convert.ToDouble(textBox1.Text);
-            sec = Convert.ToDouble(textBox2.Text);
-            sum = first + sec;
-            textBox3.Text = sum.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            double sum;
-            double first, sec;
-            first = Convert.ToDouble(textBox1.Text);
-            sec = Convert.ToDouble(textBox2.Text);
-            sum = first - sec;
-            textBox3.Text = sum.ToString();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            double sum;
-            double first, sec;
-            first = Convert.ToDouble(textBox1.Text);
-            sec = Convert.ToDouble(textBox2.Text);
-            sum = first * sec;
-            textBox3.Text = sum.ToString();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            double sum;
-            double first, sec;
-            first = Convert.ToDouble(textBox1.Text);
-            sec = Convert.ToDouble(textBox2.Text);
-            sum = first / sec;
-            textBox3.Text = sum.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            action = B.Text;
+            switch (action)
+            {
+                case "+":
+                    
+                        first = Convert.ToDouble(textBox1.Text);
+                        sec = Convert.ToDouble(textBox2.Text);
+                        sum = first + sec;
+                        textBox3.Text = sum.ToString();
+                        break;
+                case "-":
+
+                    first = Convert.ToDouble(textBox1.Text);
+                    sec = Convert.ToDouble(textBox2.Text);
+                    sum = first - sec;
+                    textBox3.Text = sum.ToString();
+                    break;
+                case "*":
+
+                    first = Convert.ToDouble(textBox1.Text);
+                    sec = Convert.ToDouble(textBox2.Text);
+                    sum = first * sec;
+                    textBox3.Text = sum.ToString();
+                    break;
+                case "/":
+
+                    first = Convert.ToDouble(textBox1.Text);
+                    sec = Convert.ToDouble(textBox2.Text);
+                    sum = first / sec;
+                    textBox3.Text = sum.ToString();
+                    break;
+                default:
+                    throw new Exception("Unknown operation");
+
+            }
         }
     }
 }
