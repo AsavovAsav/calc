0bb6a22e4e93daa484310ffe7677892365ec5d15
Dmitry <108071815+EN3RGGY@users.noreply.github.com>
2022-06-30 11:18:35 +0600

    Add files via upload

---
+++ NUnitTest1/UnitTest1.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace NUnitTest1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void UTest1()
+        {
+            Assert.Pass();
+        }
+
+        public void UTest2()
+        {
+            Assert.Pass();
+        }
+    }
+}
